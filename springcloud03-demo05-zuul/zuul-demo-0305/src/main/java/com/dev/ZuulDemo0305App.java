2be11e2b1429d404cab9fd52d580ddb7f7e96576
xiaotian.huang <hsiaotien@163.com>
2018-10-23 01:25:51 +0800

    核心组件zuul动态路由，鉴权

--- springcloud03-demo05-zuul/zuul-demo-0305/src/main/java/com/dev/ZuulDemo0305App.java
+++ springcloud03-demo05-zuul/zuul-demo-0305/src/main/java/com/dev/ZuulDemo0305App.java
@@ -3,13 +3,11 @@ package com.dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient// 面向服务的路由配置，需要启用eureka功能，进行服务的拉取
-@EnableHystrix //开启熔断功能，zuul也是看作消费者
 public class ZuulDemo0305App {
 
 	public static void main(String[] args) {
